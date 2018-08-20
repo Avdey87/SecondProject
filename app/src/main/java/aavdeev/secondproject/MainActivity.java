e2e1e6f8393427bf9009f729441e161bd67b0119
Avdey87 <avdey87@gmail.com>
2018-08-20 21:06:05 +0300

    Initial commit

---
+++ app/src/main/java/aavdeev/secondproject/MainActivity.java
@@ -0,0 +1,52 @@
+package aavdeev.secondproject;
+
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.zip.Inflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+                 switch (item.getItemId()) {
+            case R.id.settings:
+                Toast.makeText(this, R.string.settings, Toast.LENGTH_SHORT).show();
+
+                break;
+            case R.id.search:
+                Toast.makeText(this, R.string.search, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.exit:
+                Toast.makeText(this, R.string.exit, Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
